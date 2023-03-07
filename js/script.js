7731ac50210adb88bc949f162c48e8d1b21d87b2
kanvzt0 <rm98919@fiap.com.br>
2023-03-07 11:49:15 -0300

    script.js adicionado ao html.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Este é um comentário de linha
+
+/**
+ * É um comentario
+ * de
+ * Bloco
+ */
+
+//Exemplo de funções do console
+console.log("Olá Mundo!")
+console.error("Bom dia!")
+console.warn("Será?")
+console.info("Vai ser...")
